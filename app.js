dc548d5b161c9f39ccb63d34b3ae99d800978748
caroline portela <carol10.portela12@gmail.com>
2023-02-10 13:05:48 -0300

    notas

---
+++ app.js
@@ -0,0 +1,57 @@
+'use strict'
+
+/* Quando apertar em calcular deverá aparecer a situaçao do aluno*/
+
+const calcular = document.getElementById('calcular')
+
+function media() {
+    const nota1 = Number(document.getElementById('nota1').value)
+    const nota2 = Number(document.getElementById('nota2').value)
+    const formulario = document.getElementById('formulario')
+
+    const situacao = (Number(nota1) + Number(nota2))/2
+    
+    document.getElementById('situacao').value = situacao
+    
+
+    const resultadoAprovado = 'Aluno Aprovado'
+    const resultadoReprovado = 'Aluno Reprovado'
+
+    if(situacao >=5){
+        document.getElementById('situacao').value = resultadoAprovado 
+        formulario.classList.remove('sombra-vermelha')
+        formulario.classList.add('sombra-verde')
+
+    }else {
+        document.getElementById('situacao').value = resultadoReprovado
+        formulario.classList.add('sombra-verde')
+        formulario.classList.remove('sombra-vermelha')
+
+        
+    }
+}
+
+calcular.addEventListener('click', media)
+
+/*
+const calcular = document.getElementById('calcular')
+
+function somar() {
+    const numero1 = Number(document.getElementById('numero1').value)
+    const numero2 = Number(document.getElementById('numero2').value)  
+    const formulario = document.getElementById('formulario')
+
+    const resultado = Number (numero1) + Number (numero2)
+    document.getElementById('resultado').value = resultado
+    formulario.classList.add('sombra-vermelha')
+      
+}
+
+//quando clicar aqui irá somar.
+calcular.addEventListener('click', somar)
+ */
+
+
+
+
+
